0ca5c4f5ee510fd1e9e9d3959246ed5e3b75b06d
Felipe Augusto S Salvador <felipe.salvador@agrof2.com.br>
2022-08-23 22:42:16 -0300

    login component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import LoginForm from "./Components/LoginForm";
 
 function App() {
+  const adminUser ={ 
+    login: 'admin@agrof2sistemas.com', 
+    password: 'sucessoEMerito'
+  }
+
+  const [user, setUser] = useState({ login: "", senha: ""})
+  const [error, setError] = useState("")
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.login == adminUser.login && details.password == adminUser.password) {
+      console.log("Logged in!");
+      setUser({
+        login: details.login, 
+        senha: details.password
+      })
+      setError("")
+    } else {
+      console.log("Details do not match!");
+      setError("Details do not match!");
+    }
+  }
+
+  const Logout = () => {
+    console.log("Logout!");
+    setUser({ login: "", senha: "" })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.login) ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.login}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
---
+++ src/Components/LoginForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function LoginForm({ Login, error }) {
+    const [details, setDetails] = useState({ login: "", password: "" })
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        Login(details);
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-inner">
+                <h2>Login</h2>
+                {error ? ( <div className='error'>{error}</div>) : ""}
+                <div className="form-group">
+                    <label htmlFor='login'>Login:</label>
+                    <input type="email" name="login" id="login" onChange={e => setDetails({...details, login: e.target.value})} value={details.login} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor='password'>Senha:</label>
+                    <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
+                </div>
+                <input type={"submit"} value="Login" />
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
